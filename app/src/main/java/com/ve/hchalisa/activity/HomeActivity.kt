7a5eab13a8ec769e80a12643a6c8508acd62a69d
VEPallavi <pallavisinha@virtualemployee.com>
2022-06-23 11:57:47 +0530

    Initial commit

---
+++ app/src/main/java/com/ve/hchalisa/activity/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.ve.hchalisa.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ve.hchalisa.R
+
+
+class HomeActivity: AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+
+}
---
+++ app/src/main/java/com/ve/hchalisa/activity/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.ve.hchalisa.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ve.hchalisa.R
+
+
+class SplashActivity: AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+
+}
